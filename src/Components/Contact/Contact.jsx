c2d0526f4c69d492821328d8b75d0f47e624134f
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-09-21 22:05:53 +0600

    router starts

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <div>
+            <p>Contact on: ishtiak.sparrow98@gmail.com</p>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <div>
+        <ul>
+            <li>Bangladesh</li>
+            <li>Nepal</li>
+            <li>Bhutan</li>
+            <li>Australia</li>
+        </ul>
+      </div>
+    );
+};
+
+export default Home;
