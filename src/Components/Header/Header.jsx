3848cc627a0a30f556b147b85a105481454274e0
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-09-23 00:41:06 +0600

    Active route, Loading Spinner, useLocation, useNavigation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './header.css'
 const Header = () => {
     return (
         <div>
             <nav>
                 <Link to="/">Home</Link>
-                <Link to="/posts">Posts</Link>
-                <Link to="/users">Users</Link>
+                <NavLink to="/posts">Posts</NavLink>
+                <NavLink to="/users">Users</NavLink>
                 <Link to="/contact">Contact</Link>
                 <Link to="/about">About</Link>
             </nav>
